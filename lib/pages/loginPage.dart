720c0e2861c9509a4e9c99aab6d8310b82c0c735
Gabriel santi <santisjp@live.com>
2019-09-01 21:12:16 -0300

    Autenticação adicionada

--- lib/pages/loginPage.dart
+++ lib/pages/loginPage.dart
@@ -1,16 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:pucpay_prototype/pages/homePage.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
   
   _showLogo(){
   return Container(
     child: Image.asset("assets/logo_t.png",width: 1000.0 ,height: 250.0,)
   );
 }
-  
+
+final _login = TextEditingController();
+final _pass = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       body: Padding(
         padding: EdgeInsets.only(
           top: 60,
@@ -21,6 +34,7 @@ class LoginPage extends StatelessWidget {
           children: <Widget>[
             _showLogo(),
             TextFormField(
+              controller: _login,
             //autofocus: true,
               style: new TextStyle(color: Colors.black, fontSize: 15),
               decoration: InputDecoration(
@@ -29,6 +43,7 @@ class LoginPage extends StatelessWidget {
           ),
             Divider(height: 10, color: Colors.white),
             TextFormField(
+              controller: _pass,
              obscureText: true,
              style: new TextStyle(color: Colors.black, fontSize: 15),
               decoration: InputDecoration(
@@ -39,7 +54,9 @@ class LoginPage extends StatelessWidget {
               child: Row(
                 children: <Widget>[
                 FlatButton(
-                 onPressed: () {},
+                 onPressed: () {
+
+                 },
                   child: const Text(
                   'Esqueci a senha',
                   style: TextStyle(fontSize: 10)
@@ -50,7 +67,9 @@ class LoginPage extends StatelessWidget {
                 height: 0,
               ),
               RaisedButton(
-                onPressed: () {},
+                onPressed: () {
+                  doLogin(context, _login.text, _pass.text,_scaffoldKey);
+                },
                 child: const Text(
                   'LOGIN',
                   style: TextStyle(fontSize: 10)
@@ -65,4 +84,33 @@ class LoginPage extends StatelessWidget {
     ),
     );
   }
+}
+
+void doLogin(BuildContext context,login,pass,_key) async{
+  try{
+   /* FirebaseUser user = (await FirebaseAuth.instance
+      .signInWithEmailAndPassword(email: 'santisjp@gmail.com', password: '123456')).user;*/
+    print('EMAIL é: ${login}');
+    print('SENHA é: ${pass}');
+    FirebaseUser user = (await FirebaseAuth.instance
+      .signInWithEmailAndPassword(email: login, password: pass)).user;
+
+      /*if(user != null){
+        _pass.text = '';
+        Navigator.pop(context);
+      }*/
+      print('Signed In: ${user.uid}');
+      _key.currentState.showSnackBar(SnackBar(
+        content: Text("OKAY"),
+        backgroundColor: Colors.green,
+      ));
+
+  } catch(e){
+    print("Error: ${e.toString()}");
+    _key.currentState.showSnackBar(SnackBar(
+        content: Text("ERRO Verefique seu Login"),
+        backgroundColor: Colors.redAccent,
+      ));
+  }
+  FocusScope.of(context).requestFocus(new FocusNode());
 }
